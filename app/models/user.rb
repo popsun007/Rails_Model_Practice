46c089ffcd8da060fba30442b7f75b9db67bcccb
Frank Sun <popsun2010@gmail.com>
2015-10-14 09:54:43 -0700

    Add validation for first_name, last_name length, and for age must between 10 to 150 years old

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :first_name, :last_name, presence: true, length: { minimum: 2 }
+  validates :age, numericality: { greater_than: 9, less_than: 150 }
 end
